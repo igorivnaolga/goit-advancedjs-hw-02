b718b1fc6ec5a55589ced1df159c298cc2cd2217
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-01-21 01:55:16 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const page = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.disabled = true;
+
+const handlerStart = event => {
+  const interval = setInterval(() => {
+    page.style.backgroundColor = getRandomHexColor();
+
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+
+    const handlerStop = event => {
+      clearInterval(interval);
+      stopBtn.disabled = true;
+      startBtn.disabled = false;
+    };
+    stopBtn.addEventListener('click', handlerStop);
+  }, 1000);
+};
+
+startBtn.addEventListener('click', handlerStart);
